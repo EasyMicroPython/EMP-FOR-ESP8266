71fba9b211a0035d98edeb35f9260c8c4b15f3c9
fuermohao <fuermohao@outlook.com>
2018-12-20 17:53:10 +0800

    init

---
+++ README.txt
@@ -0,0 +1,1 @@
+# EMP(Easy MicroPython) is a upy module to make things Easy on MicroPython.
---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'emp-webrepl',      
+    version = '0.0.1',
+    # py_modules = ['emp_wifi','emp_boot','emp_ide','emp_utils','emp_webrepl'],
+    py_modules = ['emp_webrepl'],
+    author = 'fuermohao@1zlab.com',        
+    author_email = 'fuermohao@outlook.com',
+    url = 'http://emp.1zlab.com',
+    description = 'EMP(Easy MicroPython) is a upy module to make things Easy on MicroPython.'   
+    )
