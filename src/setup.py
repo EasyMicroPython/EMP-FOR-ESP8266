be6f2144dd5dbe13370c456df526aa9d4cadc7ef
Fuermohao <fuermohao@outlook.com>
2018-12-22 00:09:27 +0800

    version 0.1.1

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='emptool',
-    version='0.1.0',
+    version='0.1.1',
     packages=['emptool'],
     include_package_data=True,
     license='MIT License',
